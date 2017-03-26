9144cd223e7a5808695062f71fda97f2eaecca39
Ishita Mathur <ishita.mathur@delhivery.com>
2017-03-27 00:07:16 +0530

    Modified fibonacci algorithm to make it more efficient

--- week2/fibonacci/fibonacci.py
+++ week2/fibonacci/fibonacci.py
@@ -1,9 +1,14 @@
 # Uses python3
-def calc_fib(n):
-    if (n <= 1):
-        return n
+import sys
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
+n = int(sys.argv[1])
+prev, curr = 0, 1
 
-n = int(input())
-print(calc_fib(n))
+for i in range(2,n+1):
+    new = curr + prev
+    prev = curr
+    curr = new
+
+print(new)
+
+
