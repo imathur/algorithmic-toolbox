35d242b397f770486dde47ca71fbce623aed180d
Ishita Mathur <ishita.mathur@delhivery.com>
2017-03-27 00:50:03 +0530

    Added function to compute GCD using more efficiently

--- week2/fibonacci_last_digit/fibonacci_last_digit.py
+++ week2/fibonacci_last_digit/fibonacci_last_digit.py
@@ -12,8 +12,17 @@ def get_fibonacci_last_digit_naive(n):
         previous, current = current, previous + current
 
     return current % 10
+    
+def get_fibonacci_last_digit(n):
+    prev, curr = 0, 1
+
+    for i in range(2,n+1):
+        new = (curr + prev) % 10
+        prev = curr
+        curr = new
+    
+    return new
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(get_fibonacci_last_digit_naive(n))
+    n = int(sys.argv[1])
+    print(get_fibonacci_last_digit(n))
