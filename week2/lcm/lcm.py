88226a27dc45339f6a33d1277e01cb3659b96787
Ishita Mathur <ishita.mathur@delhivery.com>
2017-03-29 00:41:46 +0530

    Added function to compute lcm more efficiently

--- week2/lcm/lcm.py
+++ week2/lcm/lcm.py
@@ -7,9 +7,20 @@ def lcm_naive(a, b):
             return l
 
     return a*b
+    
+def gcd_euclidean(a, b):
+    if b == 0:
+        return a
+    a_ = a % b
+    return gcd_euclidean(b, a_)
+
+def lcm_efficient(a, b):
+    gcd = gcd_euclidean(a, b)
+    lcm = a*b//gcd
+    return lcm
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     a, b = map(int, input.split())
-    print(lcm_naive(a, b))
+    print(lcm_efficient(a, b))
 
