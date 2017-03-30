6443987d6aa8ba407ed25d117846752600a31a93
Ishita Mathur <ishita.mathur@delhivery.com>
2017-03-30 10:28:18 +0530

    Added logic for fib(n) % m, need to debug code.

--- week2/fibonacci_huge/fibonacci_huge.py
+++ week2/fibonacci_huge/fibonacci_huge.py
@@ -24,29 +24,45 @@ def calc_fib_efficient(n):
     return new
 
 def get_pisano_period(n, m):
-    fib_numbers = [0, 1]
-    modulos = [0, 1]
-    period = 2
+    '''
+    Lemma: (a + b) % m = [(a % m) + (b % m)] % m
+    F(i+2) = F(i) + F(i+1)
+    F(i+2) % m = [F(i) + F(i+1)] % m
+    F(i+2) % m = [(F(i) % m) + (F(i+1) % m)] % m
+    For i = 0: F(2) % m = [(F(0) % m) + (F(1) % m)] % m
+               F(2) % m = [0 + 1] % m
+               F(2) % m = 1
+    If F(i) % m = 0 and F(i+1) % m = 1: F(i+2) % m = 1, and so on.
+    Thus need to find the value of i where the above condition is valid.
+    period = i that satisfies condition.
+    '''
+    fib_numbers = [0, 1, 1]
+    modulos = [0, 1, 1]
+    period = None
+     
+    for i in range(2): print('i= {}\tfib= {}\tmod{}= {}'.format(i, fib_numbers[i], m, modulos[i]))
     
-    for i in range(2, n+1,2):
-        print(i)
-        
-        fib_numbers[i] = fib_numbers[i-1] + fib_numbers[i-2]
-        modulos[i] = fib_numbers[i] % m
-        
-        if i < 4:
-            continue
-        if i % 2 == 0:
-            period = i//2
-            if arr[:period] == arr[period:]:
-                break
+    for i in range(2, n+1, 1):
+        fib_numbers.append(fib_numbers[i-1] + fib_numbers[i-2])
+        modulos.append(fib_numbers[i] % m)
+        print('i= {}\tfib= {}\tmod{}= {}'.format(i, fib_numbers[i], m, modulos[i]))
+        if fib_numbers[i] == 1 and fib_numbers[i-1] == 0:
+            period = i-1
+            break
     return period
+    
 
 def get_fibonacci_huge_efficient(n, m):
     period = get_pisano_period(n, m)
-    n_ = n % period
+    if period:
+        n_ = n % period
+    else:
+        n_ = n
     fib_small = calc_fib_efficient(n_)
     mod = fib_small % m
+    
+    print('period={}, n_={}, fib_small={}'.format(period, n_, fib_small))
+    
     return mod
 
 n, m = int(sys.argv[1]), int(sys.argv[2])
@@ -58,3 +74,5 @@ if __name__ == '__main__':
     n, m = map(int, input.split())
     print(get_fibonacci_huge_naive(n, m))
 '''
+
+
