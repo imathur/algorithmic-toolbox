8a3101b19086f8bb750e8a8687b4fd9ccadb69b5
Ishita Mathur <ishita.mathur@delhivery.com>
2017-04-16 00:13:32 +0530

    Added function to calculate change in optimal way

--- week3/change/change.py
+++ week3/change/change.py
@@ -3,8 +3,19 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+    total = 0
+    if m < 5:
+    	total = m
+    elif m < 10:
+    	total = 1 + (m % 5)
+    else:
+    	total = m//10
+    	m = m % 10
+    	total += m//5 + m%5
+
+    return total
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
+    # m = int(sys.argv[1])
     print(get_change(m))
