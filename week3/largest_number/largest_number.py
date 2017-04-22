42e72b6af0c7344c7295c3f6f405e858105a9068
Ishita Mathur <ishita.mathur@delhivery.com>
2017-04-22 17:43:05 +0530

    Added function to create largest number from a string of digits in optimal way

--- week3/largest_number/largest_number.py
+++ week3/largest_number/largest_number.py
@@ -1,13 +1,29 @@
 #Uses python3
 
 import sys
+import numpy as np
+
+def is_greater_than(n, m):
+	n = str(n)
+	m = str(m)
+
+	return n+m > m+n
+
 
 def largest_number(a):
-    #write your code here
-    res = ""
-    for x in a:
-        res += x
-    return res
+    # return ''.join(sorted(a)[::-1])
+
+    res = ''
+
+    while len(a) > 0:
+    	max_num = '0'
+    	for num in a:
+    		if is_greater_than(num, max_num): # num > max_num:
+    			max_num = num
+    	res += max_num
+    	a.pop(a.index(max_num))
+
+    return ''.join(map(str, res))
 
 if __name__ == '__main__':
     input = sys.stdin.read()
