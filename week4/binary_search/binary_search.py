a251d4966ea402fb995a8c4dd238dde514bd8ea3
Ishita Mathur <ishita.mathur@delhivery.com>
2017-04-22 21:43:19 +0530

    Gotten basic logic of code, but need to correct errors

--- week4/binary_search/binary_search.py
+++ week4/binary_search/binary_search.py
@@ -1,9 +1,44 @@
 # Uses python3
 import sys
 
-def binary_search(a, x):
-    left, right = 0, len(a)
+def binary_search(b, x):
+    left, right = 0, len(b)
     # write your code here
+    mid = left
+    while left < right:
+        mid = (left+right)//2
+        if x == b[mid]:
+            return mid
+        elif x > b[mid]:
+            left = mid+1
+        else:
+            right = mid
+    return mid
+
+def combined_binary_search(a, b, found):
+    if len(a) > 0:
+        mid_a = (len(a))//2
+        mid_b = binary_search(b, a[mid_a])
+
+        b_firsthalf_upper = b_secondhalf_lower = mid_b + 1
+        
+        if a[mid_a] == b[mid_b]:
+            found[mid_b] = mid_a
+            b_firsthalf_upper -= 1
+            
+        
+        found[:b_firsthalf_upper] = combined_binary_search(
+                                        a[:mid_a],
+                                        b[:b_firsthalf_upper], 
+                                        found[:b_firsthalf_upper]
+                                    )
+        found[b_secondhalf_lower:] = combined_binary_search(
+                                         a[mid_a:],
+                                         b[b_secondhalf_lower:],
+                                         found[b_secondhalf_lower:]
+                                     )
+        return found
+
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -17,6 +52,11 @@ if __name__ == '__main__':
     n = data[0]
     m = data[n + 1]
     a = data[1 : n + 1]
-    for x in data[n + 2:]:
-        # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+    b = sorted(data[n + 2:])
+    found = [-1 for _ in range(len(b))]
+    # for x in b:
+    #     # replace with the call to binary_search when implemented
+    #     print(binary_search(a, x), end = ' ')
+
+    found = combined_binary_search(a, b, found)
+    print(' '.join([str(each) for each in found]))
