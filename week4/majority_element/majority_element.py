1530ef902368c04b074262366193991d9a4ecfaf
Ishita Mathur <ishita.mathur@delhivery.com>
2017-04-27 22:26:02 +0530

    Added efficient algorithm for majority element problem

--- week4/majority_element/majority_element.py
+++ week4/majority_element/majority_element.py
@@ -1,13 +1,12 @@
 # Uses python3
 import sys
+from collections import Counter
 
 def get_majority_element(a, left, right):
-    if left == right:
+    if Counter(a).most_common(1)[0][1] > len(a)/2:
+        return 0
+    else:
         return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
-    return -1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
