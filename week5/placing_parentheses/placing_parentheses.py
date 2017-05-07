a079416f84fb3ca6f8c8c5c609f5e20f4387862a
Ishita Mathur <ishita.mathur@delhivery.com>
2017-05-07 21:58:01 +0530

    Added functions for calculating maximum value of arithmetic expression

--- week5/placing_parentheses/placing_parentheses.py
+++ week5/placing_parentheses/placing_parentheses.py
@@ -1,4 +1,5 @@
 # Uses python3
+
 def evalt(a, b, op):
     if op == '+':
         return a + b
@@ -9,10 +10,40 @@ def evalt(a, b, op):
     else:
         assert False
 
-def get_maximum_value(dataset):
-    #write your code here
-    return 0
+def MinMax(i, j, op, m, M):
+    import numpy as np
+    m_min = np.inf
+    m_max = -np.inf
 
+    for k in range(i, j):
+        a = evalt(M[i][k], M[k+1][j], op[k])
+        b = evalt(M[i][k], m[k+1][j], op[k])
+        c = evalt(m[i][k], M[k+1][j], op[k])
+        d = evalt(m[i][k], m[k+1][j], op[k])
+        
+        m_min = min(m_min, a, b, c, d)
+        m_max = max(m_max, a, b, c, d)
+    
+    return(m_min, m_max)      
+        
+def get_maximum_value(dataset):
+    op = dataset[1 : len(dataset) : 2]
+    digits = dataset[0 : len(dataset)+1 : 2]
+
+    n = len(digits)
+    m = [[0]*n for _ in range(n)]
+    M = [[0]*n for _ in range(n)]
+
+    for i in range(n):
+        m[i][i] = int(digits[i])
+        M[i][i] = int(digits[i])
+
+    for s in range(1, n):
+        for i in range(n-s):
+            j = i + s
+            m[i][j], M[i][j] = MinMax(i, j, op, m, M)
+
+    return M[0][n-1]
 
 if __name__ == "__main__":
     print(get_maximum_value(input()))
