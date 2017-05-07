c4a51d00a2a8a91ba7789fa445b9cc9fb6ab0e55
Ishita Mathur <ishita.mathur@delhivery.com>
2017-05-07 13:46:26 +0530

    Modified function to optimize calculation of edit distance

--- week5/edit_distance/edit_distance.py
+++ week5/edit_distance/edit_distance.py
@@ -1,7 +1,27 @@
 # Uses python3
 def edit_distance(s, t):
-    #write your code here
-    return 0
+    n = len(s)
+    m = len(t)
+    
+    d = [[0]*(m+1) for _ in range(n+1)] 
+    
+    for i in range(0, n+1):
+        d[i][0] = i
+    
+    for i in range(0, m+1):
+        d[0][i] = i
+    
+    cost = 0
+    
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if s[i-1] == t[j-1]:
+                cost = 0
+            else:
+                cost = 1
+            d[i][j] = min(min(d[i-1][j], d[i][j-1])+1, d[i-1][j-1]+cost)
+    
+    return d[n][m]
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
