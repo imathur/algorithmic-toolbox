e6e6549cb13c43b5c04437dcbba19287edc93f11
Ishita Mathur <ishita.mathur@delhivery.com>
2017-05-07 22:00:56 +0530

    Added function to calculate length of longest common subsequence

--- week5/lcs3/lcs3.py
+++ week5/lcs3/lcs3.py
@@ -3,8 +3,23 @@
 import sys
 
 def lcs3(a, b, c):
-    #write your code here
-    return min(len(a), len(b), len(c))
+    m = len(a)
+    n = len(b)
+    p = len(c)
+    
+    L = [[[None for k in range(p+1)] for j in range(n+1)] for i in range(m+1)]
+    
+    for i in range(m+1):
+        for j in range(n+1):
+            for k in range(p+1):
+                if (i == 0 or j == 0 or k == 0):
+                    L[i][j][k] = 0
+                elif(a[i-1] == b[j-1] and a[i-1] == c[k-1]):
+                    L[i][j][k] = L[i-1][j-1][k-1] + 1
+                else:
+                    L[i][j][k] = max(L[i-1][j][k], L[i][j-1][k], L[i][j][k-1], L[i-1][j-1][k], L[i-1][j][k-1], L[i][j-1][k-1])
+    
+    return L[m][n][p]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
