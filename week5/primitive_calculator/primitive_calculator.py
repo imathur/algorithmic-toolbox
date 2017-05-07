4df2b39e8244e25367a0f08343ee53d05fe53f49
Ishita Mathur <ishita.mathur@delhivery.com>
2017-05-07 12:45:22 +0530

    Modified function implement primitive calculator problem using DP

--- week5/primitive_calculator/primitive_calculator.py
+++ week5/primitive_calculator/primitive_calculator.py
@@ -1,16 +1,41 @@
 # Uses python3
 import sys
 
+# def optimal_sequence(n):
+#     sequence = []
+#     while n >= 1:
+#         sequence.append(n)
+#         if n % 3 == 0:
+#             n = n // 3
+#         elif n % 2 == 0:
+#             n = n // 2
+#         else:
+#             n = n - 1
+#     return reversed(sequence)
+
 def optimal_sequence(n):
     sequence = []
-    while n >= 1:
+        
+    a = [0]*(n+1)
+    
+    for each in range(1, len(a)):
+        a[each] = a[each-1] + 1
+        if each % 2 == 0:
+            a[each] = min(1+a[each//2], a[each])
+        if each % 3 == 0:
+            a[each] = min(1+a[each//3], a[each])
+    
+    while n > 1:
         sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
-        else:
+        if a[n-1] == a[n]-1:
             n = n - 1
+        elif (n % 2 == 0 and a[n//2] == a[n]-1):
+            n = n//2     
+        elif (n % 3 == 0 and a[n//3] == a[n]-1):
+            n = n//3
+    
+    sequence.append(1)
+    
     return reversed(sequence)
 
 input = sys.stdin.read()
