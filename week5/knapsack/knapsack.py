e93d2cc571fc706d70cb4fc7614917b9e1a2e777
Ishita Mathur <ishita.mathur@delhivery.com>
2017-05-07 13:33:34 +0530

    Added optimized code for knapsack problem

--- week5/knapsack/knapsack.py
+++ week5/knapsack/knapsack.py
@@ -1,13 +1,20 @@
 # Uses python3
 import sys
 
+# def optimal_weight(W, w):
+#     # write your code here
+#     result = 0
+#     for x in w:
+#         if result + x <= W:
+#             result = result + x
+#     return result
+
 def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    c = [0]*(W+1)
+    for i in range(len(w)):
+        for j in range(W, w[i]-1, -1):
+            c[j] = max(c[j], c[j-w[i]]+w[i])
+    return c[W]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
